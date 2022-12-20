4085248b7cdf0bc11f0fb772d34812e94d32710a
pckabeer <pckabeer@gmail.com>
2022-12-20 16:16:15 +0400

    trimmed

--- parsing/parse.c
+++ parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: kpanikka <kpanikka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 17:05:07 by kpanikka          #+#    #+#             */
-/*   Updated: 2022/12/17 13:54:20 by kpanikka         ###   ########.fr       */
+/*   Updated: 2022/12/20 13:42:29 by kpanikka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,28 +86,25 @@ void	parse(void)
 	parse_count_pipe();
 	g_msv.cmd_arr = ft_split(g_msv.rline, 2);
 	g_msv.i = -1;
-//	printf("num pipes : %d \n", g_msv.num_pipe + 1);
+//	printf("num blocks : %d \n", g_msv.num_pipe + 1);
 	g_msv.cmd_block_arr = calloc(sizeof(t_cblock), g_msv.num_pipe + 1);
 	cbd = g_msv.cmd_block_arr;
 	while (g_msv.cmd_arr[++g_msv.i])
 	{
 		if (g_msv.i >= 1)
 			cbd[g_msv.i - 1].next = &cbd[g_msv.i];
-			
 		init_t_cblock(&cbd[g_msv.i]);
 		g_msv.cmd_arr[g_msv.i] = ft_strtrim(g_msv.cmd_arr[g_msv.i], " ");
 		tblock_counter(&cbd[g_msv.i], g_msv.cmd_arr[g_msv.i]);
 		cbd[g_msv.i].input = ft_split(cbd[g_msv.i].input_h, 2);
 		cbd[g_msv.i].output = ft_split(cbd[g_msv.i].output_h, 2);
 		cbd[g_msv.i].cmd = ft_split(cbd[g_msv.i].cmd_h, 2);
-
-		
 		expand();
-	//printf("input : %d -- output : %d  --command : %d\n",
-		//	cbd[g_msv.i].input_ctr,
-	//		cbd[g_msv.i].output_ctr,
-	//		cbd[g_msv.i].cmd_ctr);
-	//		int j =-1;
+		printf("input : %d -- output : %d  --command : %d\n",
+			cbd[g_msv.i].input_ctr,
+			cbd[g_msv.i].output_ctr,
+			cbd[g_msv.i].cmd_ctr);
+			// int j =-1;
 			/*while (cbd[g_msv.i].input[++j])
 			{
 				printf(" input -- %s\n",cbd[g_msv.i].input[j]);
